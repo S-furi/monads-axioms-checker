df21b09c50351e167b7f782c04e87a82f79f08d4
S-furi <stefano.furi@studio.unibo.it>
2025-06-13 12:02:53 +0200

    feat: add custom `Optional` support

---
+++ src/test/scala/MonadOptionalCheck.scala
@@ -0,0 +1,56 @@
+package scala
+
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.{Arbitrary, Gen, Properties}
+
+import scala.Monads.Monad
+import scala.types.Optionals.Optional
+import scala.types.Optionals.Optional.{Empty, Just}
+
+object MonadOptionalCheck:
+
+  val optionalMonadLaws: Properties = MonadLaws.laws[Optional, Int, String, Boolean](
+    name = "Optional"
+  )(using
+    summon[Monad[Optional]],
+    summon[Arbitrary[Int]],
+    summon[Arbitrary[Optional[Int]]],
+    summon[Arbitrary[Int => Optional[String]]],
+    summon[Arbitrary[String => Optional[Boolean]]],
+    optionalEq
+  )
+
+  def optionalEq[A](x: Optional[A], y: Optional[A]): Boolean = (x, y) match
+    case (Just(a1), Just(a2)) => a1 == a2
+    case (Empty(), Empty())   => true
+    case _                    => false
+
+  given arbOptInt: Arbitrary[Optional[Int]]      = optionalArbitrary[Int]
+  given arbOptStr: Arbitrary[Optional[String]]   = optionalArbitrary[String]
+  given arbOptBool: Arbitrary[Optional[Boolean]] = optionalArbitrary[Boolean]
+
+  def optionalArbitrary[A: Arbitrary]: Arbitrary[Optional[A]] = Arbitrary(
+    Gen.frequency(
+      1 -> Gen.const(Empty()),
+      9 -> arbitrary[A].map(Just(_))
+    )
+  )
+
+  given Arbitrary[Int => Optional[String]] = Arbitrary(
+    Gen.oneOf(
+      (x: Int) => Just(x.toString),
+      (x: Int) => if x % 2 == 0 then Just("even") else Empty(),
+      (_: Int) => Empty().asInstanceOf[Optional[String]]
+    )
+  )
+
+  given arbStrToOptBool: Arbitrary[String => Optional[Boolean]] = Arbitrary(
+    Gen.oneOf(
+      (s: String) => Just(s.nonEmpty),
+      (s: String) => if s.length > 5 then Just(true) else Empty(),
+      (_: String) => Empty().asInstanceOf[Optional[Boolean]]
+    )
+  )
+
+object CheckOptionalMonadLaws extends App:
+  MonadLawsRunner.runCheks("OptionalMonad")(MonadOptionalCheck.optionalMonadLaws)
---
+++ src/test/scala/types/Optionals.scala
@@ -0,0 +1,26 @@
+package scala.types
+
+import scala.Monads.Monad
+
+object Optionals:
+  enum Optional[A]:
+    case Just(a: A)
+    case Empty()
+
+  object Optional:
+    extension [A](m: Optional[A])
+      def filter(p: A => Boolean): Optional[A] = m match
+        case Just(a) if p(a) => Just(a)
+        case _ => Empty()
+
+
+  given Monad[Optional] with
+    import Optional.{Just, Empty}
+
+    def unit[A](a: A): Optional[A] = Just(a)
+
+    extension [A](m: Optional[A])
+      override def flatMap[B](f: A => Optional[B]): Optional[B] =
+        m match
+          case Just(a) => f(a)
+          case Empty() => Empty()
